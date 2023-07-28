df09b9dd0621c052b8aa928a5752404af379bdde
raselsharif <raselsharif9@gmail.com>
2023-07-28 22:48:44 +0600

    added concatination

---
+++ concatination.js
@@ -0,0 +1,31 @@
+var price1 = "10";
+var price2 = "20";
+var totalPrice = price1+price2;
+
+console.log(totalPrice);
+
+var price1 = "10";
+var price2 = 20;
+var totalPrice = price1+price2;
+
+console.log(totalPrice);
+
+var price1 = 10;
+var price2 = "20";
+price2=parseInt(price2);
+var totalPrice = price1+price2;
+
+console.log(totalPrice);
+var price1 = 10;
+var price2 = "20.5";
+price2=parseInt(price2);
+var totalPrice = price1+price2;
+
+console.log(totalPrice);
+console.log(totalPrice);
+var price1 = 10;
+var price2 = "20.5";
+price2=parseFloat(price2);
+var totalPrice = price1+price2;
+
+console.log(totalPrice);
