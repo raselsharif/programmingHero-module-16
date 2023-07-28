203ddcdd79fc645b99e86b3d27b48364e45ecb81
raselsharif <raselsharif9@gmail.com>
2023-07-28 21:21:23 +0600

    initaill commit

---
+++ variable.js
@@ -0,0 +1,19 @@
+// ======= numeric variable ====
+var age = 26;
+var number = 1961266829;
+var holdingNo = 326;
+
+console.log(age,number,holdingNo);
+
+// ========== string variable ======
+var myName ="Rasel Sharif";
+var myAddress = "Badda, Dhaka";
+console.log(myName,myAddress);
+
+// ======== boolean variable =======
+
+var isMarried = true;
+var haveChildren =true;
+var isFreelancer = false;
+console.log(isMarried,haveChildren,isFreelancer);
+
