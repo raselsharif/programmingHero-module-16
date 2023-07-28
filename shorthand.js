a79b6da1e20dfda8566359e054839bee5e9ed899
raselsharif <raselsharif9@gmail.com>
2023-07-28 22:27:47 +0600

    added shorthand

---
+++ shorthand.js
@@ -0,0 +1,27 @@
+var price = 30;
+
+price = price + 10;
+
+console.log(price);
+
+price1 = 10;
+price2 = 20;
+
+price1+=10;
+console.log(price1);
+
+price1-=10;
+console.log(price1);
+
+price1/=10;
+console.log(price1);
+
+price1*=10;
+console.log(price1);
+
+price2 ++
+console.log(price2);
+
+price2 --
+console.log(price2);
+
