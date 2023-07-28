d79da88be3fbf878de6f029851740c21862df60a
raselsharif <raselsharif9@gmail.com>
2023-07-29 00:35:45 +0600

    added task 2

---
+++ task/task02.js
@@ -0,0 +1,12 @@
+var math = 75.25;
+var bio =65;
+var chem =80;
+var phy=35.45;
+var ban = 99.50;
+
+var total = math+bio+chem+phy+ban;
+console.log("total number = " , total);
+
+var ava = total/5;
+ava=ava.toFixed(2);
+console.log("Average Number is = ",ava);
