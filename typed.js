859de727983258245df24e762ea2c56f5ecd0a38
raselsharif <raselsharif9@gmail.com>
2023-07-28 23:12:02 +0600

    added typeof

---
+++ typed.js
@@ -0,0 +1,20 @@
+var price = 10;
+console.log(typeof price);
+
+var price = "egg";
+console.log(typeof price);
+
+var price1;
+console.log(typeof price1);
+
+var price2 = 0.1;
+var price3 = 0.2;
+var total = price2 + price3;
+console.log(total);
+
+var price2 = 0.1;
+var price3 = 0.2;
+var total = price2 + price3;
+total = total.toFixed(2);
+console.log(total);
+
