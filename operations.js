1a552e5b5f376bfe6b4f7722ca383268828086f2
raselsharif <raselsharif9@gmail.com>
2023-07-28 22:01:11 +0600

    added operations

---
+++ operations.js
@@ -0,0 +1,20 @@
+var eggPrice = 145;
+var applePrice = 250;
+
+var addtotal = eggPrice+applePrice;
+var subTotal = eggPrice-applePrice;
+// console.log(subTotal);
+// console.log(addtotal);
+
+var oneEgg =12;
+var dozen = 12;
+var multiTotal = oneEgg*dozen;
+// console.log(multiTotal);
+
+var totalMoney = 5000;
+var players = 11;
+var eachPlayer= totalMoney/players;
+console.log(parseInt(eachPlayer));
+
+
+
